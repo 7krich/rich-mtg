dbf03e53e3698d52873743e3f6274d6d2454521e
Kyle Richnafsky <7krich@gmail.com>
2022-07-28 15:33:32 -0400

    revamped nav style

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,22 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 // import { capitalizeFirstLetter } from '../utils/helpers';
 import Hamburger from './Hamburger';
-import Auth from '../utils/auth';
+import NavActive from './NavActive';
+import { Transition } from 'react-transition-group';
 
 const Nav = (props) => {
 
-    const logout = event => {
-        event.preventDefault();
-        Auth.logout();
-    };
-
     const [hamburgerOpen, setHamburgerOpen] = useState(false);
+    const [isOvalShown, setIsOvalShown] = useState(false);
 
     const toggleHamburger = () => {
         setHamburgerOpen(!hamburgerOpen)
     }
 
+    //show nav on scroll below main sing up/app form
     const [show, setShow] = useState(true);
     const [lastScrollY, setLastScrollY] = useState(0);
   
@@ -48,50 +45,27 @@ const Nav = (props) => {
 
     return(
         <header className={`active ${show && 'hidden'}`}>
-            <nav className="dynamic-nav">
-
-                <div className="nav-box">
-                    <a href="/">
-                        <h2 className="title">
-                            RICH SERVICES
-                        </h2>
-                    </a>
-                
-                    <div className="nav-row dynamic-flex">
-                        {Auth.loggedIn() ? (
-                        <>
-                            <Link to="/profile" className="nav-link">Profile</Link>
-                            <Link to="/about" className="nav-link">About</Link>
-                            <a href="/" className="nav-link" onClick={logout}>
-                                Logout
-                            </a>
-                        </>
-                        ) : (
-                        <>
-                            <Link to="/login" className="nav-link">Login</Link>
-                            <Link to="/signup" className="nav-link">Signup</Link>
-                            <Link to="/about" className="nav-link">About</Link>
-                            <Link to="/news" className="nav-link">News</Link>
-                        </>
-                        )}
-                    </div>
+            <nav className="dynamic-nav"
+                onMouseEnter={() => setIsOvalShown(true)}
+                onMouseLeave={() => setIsOvalShown(false)}
+            >
+            <div className="nav-hover">
+            </div>
+            {isOvalShown && (
+                <div className="nav-oval-active">
+                    <NavActive></NavActive>
                 </div>
-            
-
+            )}
                 <div className="hamburger" onClick={toggleHamburger}>
                     <Hamburger isOpen={hamburgerOpen}/>
                 </div>
 
                 <style jsx>{`
-                @media screen and (max-width: 980px) {
+                @media screen and (max-width: 2000px) {
+                    .nav-hover {
+                        display: ${isOvalShown ? 'none' : 'inline'};
+                    }
                     .nav-row ul {
-                        display: ${hamburgerOpen ? 'inline' : 'none'};
-                }
-                    .dynamic-nav {
-                        padding-top: ${hamburgerOpen ? '40px' : '20px'};
-                        padding-bottom: ${hamburgerOpen ? '20px' : '20px'};
-                }
-                    .portfolio-pic, .title {
                         display: ${hamburgerOpen ? 'inline' : 'none'};
                 }
                     footer p {
---
+++ client/src/components/NavActive.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Auth from '../utils/auth';
+
+function NavActive ({ isOpen }) {
+
+    const logout = event => {
+        event.preventDefault();
+        Auth.logout();
+    };
+    
+    return (
+        <nav className="dynamic-nav">
+            <>
+                <div className="nav-oval" >
+                    <div className="nav-box">
+                        <a href="/">
+                            <h2 className="title">
+                                RICH SERVICES
+                            </h2>
+                        </a>
+                    
+                        <div className="nav-row dynamic-flex">
+                            {Auth.loggedIn() ? (
+                            <>
+                                <Link to="/profile" className="nav-link">Profile</Link>
+                                <Link to="/about" className="nav-link">About</Link>
+                                <a href="/" className="nav-link" onClick={logout}>
+                                    Logout
+                                </a>
+                            </>
+                            ) : (
+                            <>
+                                <Link to="/login" className="nav-link">Login</Link>
+                                <Link to="/signup" className="nav-link">Signup</Link>
+                                <Link to="/about" className="nav-link">About</Link>
+                                <Link to="/news" className="nav-link">News</Link>
+                            </>
+                            )}
+                        </div>
+                    </div>
+                </div>
+
+                <style jsx >{`
+                .burger1{
+                    transform: ${ isOpen ? 'rotate(45deg)' : 'rotate(0)'};
+                    margin-left: ${ isOpen ? '4px' : '0px'}
+                }
+                .burger2{
+                    transform: ${ isOpen ? 'scaleX(0)' : 'rotate(0)'}
+                }
+                .burger3{
+                    transform: ${ isOpen ? 'rotate(-45deg)' : 'rotate(0)'};
+                    margin-left: ${ isOpen ? '4px' : '0px'}
+                }
+
+                `}</style>
+            </>
+        </nav>
+    )
+}
+
+export default NavActive;
